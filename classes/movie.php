88515d52e041e0d6aa7dc870ba74976404d87294
marcoaufiero <marcoaufiero96@gmail.com>
2022-12-16 14:37:38 +0100

    class Movie added and 3 movies data added in index

--- classes/movie.php
+++ classes/movie.php
@@ -1,4 +1,18 @@
 <?php 
 
+class Movie {
+  public $title;
+  public $genre;
+  public $duration;
+
+  public function __construct($x , $y , $z) {
+    $this->title = $x;
+    $this->genre = $y;
+    $this->duration = $z;
+  }
+
+}
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php 
 
+include_once __DIR__ . '/classes/movie.php';
+
+$movie1 = new Movie("Batman Begins", "Azione", 140);
+$movie2 = new Movie("Ritorno al Futuro Parte I", "Fantascienza", 116);
+$movie3 = new Movie("Le Iene" , "Drammatico" , 99);
+
+var_dump($movie2)
+
+
+
 
 ?>
